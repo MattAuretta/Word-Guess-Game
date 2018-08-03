160a39891954e9894bead071672c90d3c05838c8
Matt Auretta <mauretta2@gmail.com>
2018-08-03 15:55:32 -0600

    Fully functional gameplay! Need to add images/sounds.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,12 @@
 //Create constant array of the words to be guessed
 var wordBank = ["galaxy", "eclipse", "meteor", "comet", "asteroid", "nebula", "quasar"];
 
-//Create variable that chooses words randomly
-var chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-console.log(chosenWord);
+//Create variable to hold randmoly chosen word
+var chosenWord = [];
 
-//Creates variables for wrong letters
+//Create variable for wrong letters
 var wrongLetter = [];
+var letterHolder = [];
 
 //Create variable for guesses remaining
 var remainingGuesses = 10;
@@ -14,33 +14,48 @@ var remainingGuesses = 10;
 //Create variable for wins
 var wins = 0;
 
-//Creates string of underscores based on length of chosenWord
+//Create variable for underscores
 var underscores = [];
-for (var i = 0; i < chosenWord.length; i++) {
-    underscores[i] = "_";
-}
 
-//Insert starting wins to 0 in html
+//Assign variables to IDs
 var winDiv = document.getElementById("total-wins");
-winDiv.innerHTML = wins;
 winDiv.setAttribute("class", "game");
-
-//Insert underscores into html
 var newDiv = document.getElementById("current-word");
-newDiv.innerHTML = underscores.join(" ");
 newDiv.setAttribute("class", "game");
-
-//Insert guesses remaining into html
 var guessDiv = document.getElementById("guesses-remaining");
-guessDiv.innerHTML = remainingGuesses;
 guessDiv.setAttribute("class", "game");
-
-//Insert wrongLetter array into html
 var wrongLetterDiv = document.getElementById("guessed-letters");
-wrongLetterDiv.innerHTML = wrongLetter;
 wrongLetterDiv.setAttribute("class", "game");
 
+//Create function to reset game
+function resetGame() {
+    //Chooses random string from wordBank
+    chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+    //Reset underscores array to empty
+    underscores = [];
+
+    //Display number of wins
+    winDiv.innerHTML = wins;
+
+    //Reset remaining guesses 
+    remainingGuesses = 10;
+    guessDiv.innerHTML = remainingGuesses;
+
+    //Reset wrong letters
+    letterHolder = [];
+    wrongLetter = [];
+    wrongLetterDiv.innerHTML = wrongLetter;
+
+    //Loop for creating underscores to match chosenWord length
+    for (var i = 0; i < chosenWord.length; i++) {
+        underscores[i] = "_";
+        newDiv.innerHTML = underscores.join(" ");
+    }
+}
+
 //  ===========  Start Game Loop  ===========  //
+resetGame();
 
 // Function is run whenever the user presses a key
 document.onkeyup = function (event) {
@@ -48,36 +63,42 @@ document.onkeyup = function (event) {
     //Determines which key was pressed
     var userGuess = event.key;
 
-    //Check if userGuess is included in chosenWord string
-    if (chosenWord.indexOf(userGuess) > -1) {
+    //Make sure only keys A to Z are valid
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
 
-        //Replace underscores with userGuess
-        for (var i = 0; i < chosenWord.length; i++) {
-            if (chosenWord[i] == userGuess) {
-                underscores[i] = userGuess;
+        //Check if userGuess is included in chosenWord string
+        if (chosenWord.indexOf(userGuess) > -1) {
+
+            //Replace underscores with userGuess
+            for (var i = 0; i < chosenWord.length; i++) {
+                if (chosenWord[i] == userGuess) {
+                    underscores[i] = userGuess;
+                }
+            }
+            newDiv.innerHTML = underscores.join(" ");
+
+            //If userGuess is not in chosenWord, push to wrongLetter and remove 1 guess
+        } else {
+            if (wrongLetter.indexOf(userGuess) == -1) {
+                wrongLetter.push(userGuess);
+                remainingGuesses--;
+                //Push remaining guesses and wrongLetters to HTML
+                guessDiv.innerHTML = remainingGuesses;
+                wrongLetterDiv.innerHTML = wrongLetter;
             }
         }
-        newDiv.innerHTML = underscores.join(" ");
-
-        // Alert you win if underscores are filled with correct letters
-        if (underscores.join("") == chosenWord) {
-            wins++;
-            winDiv.innerHTML = wins;
-        }
-
-        //If userGuess is not in chosenWord, push to wrongLetter and remove 1 guess
-    } else {
-        wrongLetter.push(userGuess);
-        remainingGuesses--;
     }
 
-    //Push remaining guesses and letters guessed to HTML
-    guessDiv.innerHTML = remainingGuesses;
-    wrongLetterDiv.innerHTML = wrongLetter;
+    // Add win if underscores are filled with correct letters
+    if (underscores.join("") == chosenWord) {
+        wins++;
+        winDiv.innerHTML = wins;
+        resetGame();
+    }
 
-    //Reload page when remainingGuesses are 0
+    //Reset game when remainingGuesses are 0
     if (remainingGuesses == 0) {
-        location.reload();
+        resetGame();
     }
 
 }
