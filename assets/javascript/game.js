24198967d3785893cceb17c181ee43ab7d4042be
Matt Auretta <mauretta2@gmail.com>
2018-08-04 18:05:50 -0600

    added images and sounds

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,6 @@ var chosenWord = [];
 
 //Create variable for wrong letters
 var wrongLetter = [];
-var letterHolder = [];
 
 //Create variable for guesses remaining
 var remainingGuesses = 10;
@@ -27,6 +26,12 @@ guessDiv.setAttribute("class", "game");
 var wrongLetterDiv = document.getElementById("guessed-letters");
 wrongLetterDiv.setAttribute("class", "game");
 
+//Create audio variable for winning sound
+var audio = new Audio('/sci-fi-sound.mp3');
+
+//Declare global variable for images
+var img;
+
 //Create function to reset game
 function resetGame() {
     //Chooses random string from wordBank
@@ -43,7 +48,6 @@ function resetGame() {
     guessDiv.innerHTML = remainingGuesses;
 
     //Reset wrong letters
-    letterHolder = [];
     wrongLetter = [];
     wrongLetterDiv.innerHTML = wrongLetter;
 
@@ -52,6 +56,12 @@ function resetGame() {
         underscores[i] = "_";
         newDiv.innerHTML = underscores.join(" ");
     }
+}
+
+//Create function to reset images
+function resetImage(){
+var img = document.getElementById("image");   
+img.removeChild(img.childNodes[0]);
 }
 
 //  ===========  Start Game Loop  ===========  //
@@ -93,11 +103,73 @@ document.onkeyup = function (event) {
     if (underscores.join("") == chosenWord) {
         wins++;
         winDiv.innerHTML = wins;
+
+        //Create images for correct word
+        if (underscores.join("") == "comet") {
+            img = document.createElement('img');
+            img.setAttribute("id", "win-image")
+            img.src = "assets/images/comet.gif";
+            document.getElementById("image").appendChild(img);
+            document.getElementById("win-text").innerHTML = "Comet";
+        }
+        if (underscores.join("") == "galaxy") {
+            img = document.createElement('img');
+            img.setAttribute("id", "win-image")
+            img.src = "assets/images/galaxy.gif";
+            document.getElementById("image").appendChild(img);
+            document.getElementById("win-text").innerHTML = "Galaxy";
+        }
+        if (underscores.join("") == "eclipse") {
+            img = document.createElement('img');
+            img.setAttribute("id", "win-image")
+            img.src = "assets/images/eclipse.gif";
+            document.getElementById("image").appendChild(img);
+            document.getElementById("win-text").innerHTML = "Eclipse";
+        }
+        if (underscores.join("") == "meteor") {
+            img = document.createElement('img');
+            img.setAttribute("id", "win-image")
+            img.src = "assets/images/meteor.gif";
+            document.getElementById("image").appendChild(img);
+            document.getElementById("win-text").innerHTML = "Meteor";
+        }
+        if (underscores.join("") == "asteroid") {
+            img = document.createElement('img');
+            img.setAttribute("id", "win-image")
+            img.src = "assets/images/asteroid.gif";
+            document.getElementById("image").appendChild(img);
+            document.getElementById("win-text").innerHTML = "Asteroid";
+        }
+        if (underscores.join("") == "nebula") {
+            img = document.createElement('img');
+            img.setAttribute("id", "win-image")
+            img.src = "assets/images/nebula.gif";
+            document.getElementById("image").appendChild(img);
+            document.getElementById("win-text").innerHTML = "Nebula";
+        }
+        if (underscores.join("") == "quasar") {
+            img = document.createElement('img');
+            img.setAttribute("id", "win-image")
+            img.src = "assets/images/quasar.gif";
+            document.getElementById("image").appendChild(img);
+            document.getElementById("win-text").innerHTML = "Quasar";
+        }
+
+
+        //Play winning sound
+        document.getElementById("audio-win").play();
         resetGame();
+        //Run reset function after first win image is appended
+        if(wins > 1){
+            resetImage();
+        }
     }
 
     //Reset game when remainingGuesses are 0
     if (remainingGuesses == 0) {
+        //Play losing sound
+        document.getElementById("audio-lose").play();
+        document.getElementById("win-text").innerHTML = "Try Again";
         resetGame();
     }
 
